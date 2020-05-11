de012435e8ae5d1e2cd96efddb8e6236c3bd3998
Justina Kuchta <justyna.kuchta@gmail.com>
2020-05-11 16:46:08 -0400

    changes background color on hover with @HostListener

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,14 +1,40 @@
-import { Directive, Renderer2, OnInit, ElementRef } from '@angular/core';
+// import { Directive, Renderer2, OnInit, ElementRef } from '@angular/core';
+
+// @Directive({
+//   selector: '[appBetterHighlight]'
+// })
+// export class BetterHighlightDirective implements OnInit {
+
+//   // 
+//   constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
+
+//   // renderer is a better approach of accessing the dom
+//   ngOnInit() {
+//     this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'light-blue')
+//   }
+// }
+
+// the following is an interactive version
+
+import { Directive, Renderer2, OnInit, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
 
-  // 
   constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'light-blue')
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'light-blue', false, false)
   }
+
+  @HostListener('mouseenter') mouseenter(eventData: Event) {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'goldenrod')
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'magenta')
+  }
+
 }
