e662df45dc796d1ec0433d6ce7d0472b2d1f5d48
Justina Kuchta <justyna.kuchta@gmail.com>
2020-05-11 15:29:04 -0400

    built better-highlight directive with Renderer2

--- src/app/basic-highlight/basic-highlight.directive.ts
+++ src/app/basic-highlight/basic-highlight.directive.ts
@@ -1,16 +1,22 @@
 import { Directive, ElementRef, OnInit } from "@angular/core";
 
 @Directive({
+    //  where we attach this directive
     selector: '[appBasicHighlight]'
 })
 
 // injection is an easier way to get access to other classes
 export class BasicHighlightDirective implements OnInit {
 
+    // injection and arguments you want to get every time an instance of the class is created
+    // which in this case is an elementRef. Adding "private" will automatically assign this value to the property
+    // private is a Typescript shortcat
     constructor(private elementRef: ElementRef) {
     }
 
     ngOnInit() {
+        // accessing elements like this is not a good practice (directly)
+        // see better-highlight
         this.elementRef.nativeElement.style.backgroundColor = 'green'
     }
 
